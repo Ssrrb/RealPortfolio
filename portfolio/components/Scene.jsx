3306585af387190b4ba7b5315d921c1dfdc02524
Ssrrb <sebarojasb20@gmail.com>
2025-02-10 17:23:21 -0300

    Cube not appearing, but text working

--- portfolio/components/Scene.jsx
+++ portfolio/components/Scene.jsx
@@ -1,12 +1,14 @@
 'use client';
-import { Canvas } from '@react-three/fiber'
-import { Environment } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber';
+import { Environment } from '@react-three/drei';
+import Model from './Model'; // Import your Model component
 
 export default function Index() {
   return (
-    <Canvas style={{background: '#000000'}}>
-        <directionalLight intensity={2} position={[0, 2, 3]}/>
-        <Environment preset="city" />
+    <Canvas style={{ background: '#000000' }}>
+      <directionalLight intensity={2} position={[0, 2, 3]} />
+      <Environment preset="city" />
+      <Model /> {/* Add the Model to the scene */}
     </Canvas>
-  )
+  );
 }
