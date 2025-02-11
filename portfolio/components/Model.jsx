48261d331e70272103cf8942667f4b032359fa51
Ssrrb <sebarojasb20@gmail.com>
2025-02-11 16:46:35 -0300

    Cubo funcionando, pero sin efecto de hielo

--- portfolio/components/Model.jsx
+++ portfolio/components/Model.jsx
@@ -1,10 +1,16 @@
 import React, { useRef } from 'react';
 import { useGLTF, Text } from "@react-three/drei";
 import { useFrame, useThree } from '@react-three/fiber';
-
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
 export default function Model() {
+    const loader = new GLTFLoader();
+    loader.load( '/medias/torrus.glb', function ( gltf ) {
+	scene.add( gltf.scene );
+}, undefined, function ( error ) {
+	console.error( error );
+} );
     const { nodes } = useGLTF("/medias/torrus.glb");
     const { viewport } = useThree()
     const torus = useRef(null);
@@ -18,7 +24,7 @@ export default function Model() {
             <Text position={[0, 0, -1]} fontSize={0.5} color="white" anchorX="center" anchorY="middle">
                 hello world!
             </Text>
-            <mesh ref={torus} {...nodes.Torus002}>
+            <mesh ref={torus} {...nodes.Cube}>
                 <meshBasicMaterial/>
             </mesh>
         </group>
