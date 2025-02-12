51d8e25ca92790f29998a9a17b8feca64c64101b
Ssrrb <sebarojasb20@gmail.com>
2025-02-12 15:22:16 -0300

    Cubo blanco, semi-transparente

--- portfolio/components/Model.jsx
+++ portfolio/components/Model.jsx
@@ -10,23 +10,25 @@ import React, { useRef } from 'react';
         const torus = useRef(null);
         const { nodes } = useGLTF("/medias/torrus.glb");
 
+
         const materialProps = useControls({
             thickness: { value: 0.2, min: 0, max: 3, step: 0.05 },
-            roughness: { value: 0, min: 0, max: 1, step: 0.1 },
-            transmission: {value: 1, min: 0, max: 1, step: 0.1},
-            ior: { value: 1.2, min: 0, max: 3, step: 0.1 },
+            roughness: { value: 0, min: 0, max: 0, step: 0.1 },
+            metalness: {value: 4 },
+            transmission: {value: 0.8, min: 0, max: 1, step: 0.1},
+            ior: { value: 2.3, min: 0, max: 3, step: 0.1 },
             chromaticAberration: { value: 0.02, min: 0, max: 1},
             backside: { value: true},
         })
 
         useFrame( () => {
-            torus.current.rotation.x += 0.01
-            torus.current.rotation.y += 0.01
+            torus.current.rotation.x += 0.015
+            torus.current.rotation.y += 0.015
         })
 
         return (
             <group scale={viewport.width/10} >
-                <Text  fontSize={0.5} color="black">
+                <Text  fontSize={0.7} color="black">
                     Sebastian R. Rojas
                 </Text>
                 <mesh scale={0.00001} ref={torus} {...nodes.Cube} position={[0,0,0]}>
--- portfolio/components/Scene.jsx
+++ portfolio/components/Scene.jsx
@@ -6,8 +6,8 @@ import Model from './Model'; // Import your Model component
 export default function Index() {
   return (
     <Canvas style={{ background: '#ffffff' }}>
-      <directionalLight intensity={2} position={[0, 2, 3]} />
-      <Environment preset="sunset" />
+      <directionalLight intensity={3} position={[0, 0, 0]} />
+      <Environment preset="city" />
       <Model /> {/* Add the Model to the scene */}
     </Canvas>
   );
